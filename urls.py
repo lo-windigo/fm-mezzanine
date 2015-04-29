d71b28e355766ead4d6b6e9109befed0dabd6838
Windigo <jacob@fragdev.com>
2015-04-29 16:44:41 -0700

    Homepage implementation/improvements

--- urls.py
+++ urls.py
@@ -36,7 +36,7 @@ urlpatterns += patterns("",
     # one homepage pattern, so if you use a different one, comment this
     # one out.
 
-    url("^$", direct_to_template, {"template": "pages/index.html"}, name="home"),
+    url("^$", direct_to_template, {"template": "index.html"}, name="home"),
 
     url("^herb/", include("herbs.urls", app_name="herbs", namespace="herbs")),
 
